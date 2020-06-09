08a21f8c04bb929a6f09ba6dc3478c8cc1e2bb88
Cryp70n1c-W45p <64971260+Cryp70n1c-W45p@users.noreply.github.com>
2020-06-09 09:18:49 +0000

    Add files via upload

---
+++ echo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# -*- coding: UTF-8 -*-
+
+from fbchat import log, Client
+
+email = input("Please Enter Email: ")
+password = input("Please Enter Password: ")
+
+# Subclass fbchat.Client and override required methods
+class EchoBot(Client):
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        self.markAsDelivered(thread_id, message_object.uid)
+        self.markAsRead(thread_id)
+
+        log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
+
+        # If you're not the author, echo
+        if author_id != self.uid:
+            self.send(message_object, thread_id=thread_id, thread_type=thread_type)
+
+
+client = EchoBot(email, password)
+client.listen()
